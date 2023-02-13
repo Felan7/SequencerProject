0d807011be3a38eb4bbbc394255c26f98692ecf5
Felan7 <50790924+Felan7@users.noreply.github.com>
2023-02-13 16:17:22 +0100

    Added serverside saving

--- Webserver/script.js
+++ Webserver/script.js
@@ -1,26 +1,284 @@
+const nodes = [];
 
 function linkSliderToNumberInput(sliderId, numberId) {
+  var slider = document.getElementById(sliderId);
+  var output = document.getElementById(numberId);
 
-    var slider = document.getElementById(sliderId);
-    var output = document.getElementById(numberId);
+  output.value = slider.value;
 
-    output.value = slider.value;
-
-    // Update the current slider value (each time you drag the slider handle)
-    slider.oninput = function() {
+  // Update the current slider value (each time you drag the slider handle)
+  slider.oninput = function () {
     output.value = this.value;
-    }
+  };
 
-    output.oninput = function() {
-        slider.value = this.value;
-    }
+  output.oninput = function () {
+    slider.value = this.value;
+  };
 
-    slider.ondblclick = function(){
-        slider.value = 0;
-        output.value = 0;
-    }
+  slider.ondblclick = function () {
+    slider.value = 0;
+    output.value = 0;
+  };
 }
 
-linkSliderToNumberInput("slider-value-primary", "number-value-primary")
-linkSliderToNumberInput("slider-value-secondary", "number-value-secondary")
+linkSliderToNumberInput("slider-value-primary", "number-value-primary");
+linkSliderToNumberInput("slider-value-secondary", "number-value-secondary");
 
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  connectAll("svg1");
+}
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function translateBoolToIcon(boolean) {
+  if (boolean) {
+    return "&#x2611;";
+  } else {
+    return "&#x2612;";
+  }
+}
+
+/**
+ *
+ * @param {Number} number
+ * @returns
+ */
+function formatNumber(number) {
+  var returnNumber = Math.abs(number);
+  returnNumber = returnNumber.toFixed(2);
+  var returnString = returnNumber.toString();
+
+  if (Math.abs(number) < 10) {
+    returnString = "0" + returnString;
+  }
+
+  if (number >= 0) {
+    returnString = "+" + returnString;
+  } else {
+    returnString = "-" + returnString;
+  }
+  return returnString;
+}
+
+function randomInt(upper) {
+  return Math.floor(Math.random() * upper);
+}
+function randomColor() {
+  return (
+    "rgb(" + randomInt(255) + "," + randomInt(255) + "," + randomInt(255) + ")"
+  );
+}
+
+values = { a: -12.34, b: 4.3, trigger: true, gate: false };
+function createNodeCard(id, values) {
+  var color = randomColor();
+
+  const newCard = document.createElement("div");
+
+  newCard.id = id;
+  newCard.draggable = true;
+  newCard.ondragstart = function (event) {
+    event.target.parentElement.classList.add("border");
+    drag(event);
+  };
+  newCard.onclick = function () {
+    $(".node-card").removeClass("border-primary");
+    $(".node-card").addClass("border-dark");
+    newCard.classList.remove("border-dark");
+    newCard.classList.add("border-primary");
+    switchEditWindowNode(newCard.id);
+  };
+  newCard.classList.add(
+    "node-card",
+    "col",
+    "card",
+    "m-1",
+    "border",
+    "border-dark",
+    "border-2"
+  );
+
+  const newCardHeader = document.createElement("header");
+  newCardHeader.classList.add("card-header", "p-1");
+  newCardHeader.innerHTML =
+    "<span class='badge rounded-pill' style='background-color: " +
+    color +
+    "'>&nbsp</span> " +
+    id;
+
+  const newCardBody = document.createElement("div");
+  newCardBody.classList.add("card-body", "p-1");
+  newCardBody.style.fontFamily = "monospace";
+  //create a table structured set of value displays
+  const container = document.createElement("div");
+  container.classList.add("container-fluid");
+
+  const row1 = document.createElement("section");
+  row1.classList.add("row");
+
+  const col1 = document.createElement("article");
+  col1.id = id + "-value-a";
+  col1.classList.add("col", "p-0");
+  col1.innerHTML = "A:" + formatNumber(values.a);
+
+  row1.appendChild(col1);
+
+  const col2 = document.createElement("article");
+  col2.id = id + "-value-trigger";
+  col2.classList.add("col");
+  col2.innerHTML = "T:" + translateBoolToIcon(values.trigger);
+
+  row1.appendChild(col2);
+
+  const row2 = document.createElement("section");
+  row2.classList.add("row");
+
+  const col3 = document.createElement("article");
+  col3.id = id + "-value-b";
+  col3.classList.add("col", "p-0");
+  col3.innerHTML = "B:" + formatNumber(values.b);
+
+  row2.appendChild(col3);
+
+  const col4 = document.createElement("article");
+  col4.id = id + "-value-gate";
+  col4.classList.add("col");
+  col4.innerHTML = "G:" + translateBoolToIcon(values.gate);
+
+  row2.appendChild(col4);
+
+  container.appendChild(row1);
+  container.appendChild(row2);
+
+  newCardBody.appendChild(container);
+
+  newCard.appendChild(newCardHeader);
+  newCard.appendChild(newCardBody);
+
+  return newCard;
+}
+
+const colorNames = ["1", "2", "3", "4", "5", "6"];
+colorNames.forEach((element) => {
+  document.getElementById("leRow").appendChild(createNodeCard(element, values));
+});
+
+const rowCount = 10;
+const columnCount = 6;
+for (let indexRow = 0; indexRow < rowCount; indexRow++) {
+  const newRow = document.createElement("div");
+  newRow.classList.add("row");
+  newRow.classList.add("h-100");
+  for (let indexCoulumn = 0; indexCoulumn < columnCount; indexCoulumn++) {
+    const newColumn = document.createElement("div");
+    newColumn.classList.add(
+      "col",
+      "border",
+      "p-0",
+      "m-2",
+      "rounded",
+      "border-dashed"
+    );
+
+    newColumn.style.minHeight = "4em";
+    //newColumn.innerHTML = indexRow + " " + indexCoulumn;
+    newColumn.ondrop = function () {
+      event.target.classList.remove("border");
+      drop(event);
+    };
+    newColumn.ondragover = function () {
+      allowDrop(event);
+    };
+    newRow.appendChild(newColumn);
+  }
+  document.getElementById("outer").appendChild(newRow);
+}
+
+function switchEditWindowNode(id) {
+  document.getElementById("uid").value = id;
+  readDataFromArray(id);
+}
+
+function readDataFromArray(id) {
+  var node = nodes.find((element) => {
+    return element.nodeID == id;
+  });
+
+  if (node != undefined) {
+    $("#uid").val(node.nodeID);
+    $("#number-value-primary").val(node.valueA);
+    $("#number-value-secondary").val(node.valueB);
+    $("#slider-value-primary").val(node.valueA);
+    $("#slider-value-secondary").val(node.valueB);
+    $("#value-gate").val(node.valueGate);
+    $("#value-trigger").val(node.valueTrigger);
+  } else {
+    $("#uid").val(id);
+    $("#number-value-primary").val(0);
+    $("#number-value-secondary").val(0);
+    $("#slider-value-primary").val(0);
+    $("#slider-value-secondary").val(0);
+    $("#value-gate").val(false);
+    $("#value-trigger").val(false);
+  }
+}
+
+function dataSumit() {
+  var node = {
+    nodeID: document.getElementById("uid").value,
+    valueA: document.getElementById("number-value-primary").value,
+    valueB: document.getElementById("number-value-secondary").value,
+    valueGate: document.getElementById("value-gate").checked,
+    valueTrigger: document.getElementById("value-trigger").checked,
+    nextNode: [],
+  };
+  varnodeJSON = JSON.stringify(node);
+  console.log(varnodeJSON);
+
+  //update node
+  updateNodeCard(document.getElementById("uid").value, node);
+
+  //search arry for node
+  for (let index = 0; index < nodes.length; index++) {
+    if (nodes[index].nodeID == node.nodeID) {
+      var nodeInArray = nodes[index];
+      //found the node, please update
+      nodeInArray.valueA = node.valueA;
+      nodeInArray.valueB = node.valueB;
+      nodeInArray.valueGate = node.valueGate;
+      nodeInArray.valueTrigger = node.valueTrigger;
+      //we're done here -> quiting time
+      return 0;
+    }
+  }
+  //not found -> New array entry
+  nodes.push(node);
+}
+
+/**
+ * Updates the Node Cards value
+ * @param {*} id the id of the Node Card.
+ * @param {*} nodeData The JavaScriptObject containing the data.
+ */
+function updateNodeCard(id, nodeData) {
+  $("#" + id + "-value-a").html("A:" + formatNumber(nodeData.valueA));
+  $("#" + id + "-value-b").html("B:" + formatNumber(nodeData.valueB));
+  $("#" + id + "-value-trigger").html(
+    "T:" + translateBoolToIcon(nodeData.valueTrigger)
+  );
+  $("#" + id + "-value-gate").html(
+    "G:" + translateBoolToIcon(nodeData.valueGate)
+  );
+}
+
+function writeToDevice() {
+  console.log(JSON.stringify(nodes));
+}
--- Webserver/svgDraw.js
+++ Webserver/svgDraw.js
@@ -1,107 +1,283 @@
 //helper functions, it turned out chrome doesn't support Math.sgn() newDiv
 function signum(x) {
-    return (x < 0) ? -1 : 1;
+  return x < 0 ? -1 : 1;
 }
 function absolute(x) {
-    return (x < 0) ? -x : x;
+  return x < 0 ? -x : x;
 }
 
 /**
- * 
- * @param {*} svg 
- * @param {*} path 
- * @param {*} startX 
- * @param {*} startY 
- * @param {*} endX 
- * @param {*} endY 
- * 
- * @author alojzije https://gist.github.com/alojzije/11127839
+ *
+ * @param {*} svg
+ * @param {*} path
+ * @param {*} startX
+ * @param {*} startY
+ * @param {*} endX
+ * @param {*} endY
+ *
  */
 function drawPath(svg, path, startX, startY, endX, endY) {
-    // get the path's stroke width (if one wanted to be  really precise, one could use half the stroke size)
-    var stroke = parseFloat(path.attr("stroke-width"));
-    // check if the svg is big enough to draw the path, if not, set heigh/width
-    if (svg.attr("height") <  endY)                 svg.attr("height", endY);
-    if (svg.attr("width" ) < (startX + stroke) )    svg.attr("width", (startX + stroke));
-    if (svg.attr("width" ) < (endX   + stroke) )    svg.attr("width", (endX   + stroke));
-    
-    var deltaX = (endX - startX) * 0.15;
-    var deltaY = (endY - startY) * 0.15;
-    // for further calculations which ever is the shortest distance
-    var delta  =  deltaY < absolute(deltaX) ? deltaY : absolute(deltaX);
+  // get the path's stroke width (if one wanted to be  really precise, one could use half the stroke size)
+  var stroke = parseFloat(path.attr("stroke-width"));
+  // check if the svg is big enough to draw the path, if not, set heigh/width
+  if (svg.attr("height") < endY) svg.attr("height", endY);
+  if (svg.attr("width") < startX + stroke) svg.attr("width", startX + stroke);
+  if (svg.attr("width") < endX + stroke) svg.attr("width", endX + stroke);
 
-    // set sweep-flag (counter/clock-wise)
-    // if start element is closer to the left edge,
-    // draw the first arc counter-clockwise, and the second one clock-wise
-    var arc1 = 0; var arc2 = 1;
-    if (startX > endX) {
-        arc1 = 1;
-        arc2 = 0;
+  var deltaX = Math.abs(endX - startX);
+  var deltaX2 = deltaX / 2;
+  var deltaY = Math.abs(endY - startY);
+  var deltaY2 = deltaY / 2;
+  // for further calculations which ever is the shortest distance
+  var delta = deltaY < absolute(deltaX) ? deltaY : absolute(deltaX);
+
+  // set sweep-flag (counter/clock-wise)
+  // if start element is closer to the left edge,
+  // draw the first arc counter-clockwise, and the second one clock-wise
+  var arc1 = 0;
+  var arc2 = 1;
+  if (startX > endX) {
+    arc1 = 1;
+    arc2 = 0;
+  }
+  var min = Math.min(deltaX2, deltaY2) / 2;
+  const offsetX = document.getElementById("outer").offsetWidth / 80;
+  const offsetY = document.getElementById("outer").offsetHeight / 20;
+  if (startX < endX) {
+    if (startY == endY) {
+      //case 1: straigth line from left to right
+      path.attr("d", "M" + startX + " " + startY + " H" + endX + " " + endY);
+    } else if (startY > endY) {
+      //case 2: line bent up, then right
+
+      // find shortest half line
+
+      //   console.log(
+      //     "sx=" +
+      //       startX +
+      //       " sy=" +
+      //       startY +
+      //       " ex=" +
+      //       endX +
+      //       " ey=" +
+      //       endY +
+      //       " dx=" +
+      //       deltaX +
+      //       " dy=" +
+      //       deltaY +
+      //       " min=" +
+      //       min
+      //   );
+      path.attr(
+        "d",
+        "M" +
+          startX +
+          " " +
+          startY +
+          " l" +
+          (deltaX2 - min) +
+          " " +
+          0 +
+          " q" +
+          min +
+          " " +
+          0 +
+          " " +
+          min +
+          " " +
+          -min +
+          " l" +
+          0 +
+          " " +
+          -(deltaY - 2 * min) +
+          " q" +
+          0 +
+          " " +
+          -min +
+          " " +
+          min +
+          " " +
+          -min +
+          " l" +
+          (deltaX2 - min) +
+          " " +
+          0
+      );
+    } else if (startY < endY) {
+      //case 3: line bent down, then right
+      path.attr(
+        "d",
+        "M" +
+          startX +
+          " " +
+          startY +
+          " l" +
+          (deltaX2 - min) +
+          " " +
+          0 +
+          " q" +
+          min +
+          " " +
+          0 +
+          " " +
+          min +
+          " " +
+          min +
+          " l" +
+          0 +
+          " " +
+          (deltaY - 2 * min) +
+          " q" +
+          0 +
+          " " +
+          min +
+          " " +
+          min +
+          " " +
+          min +
+          " l" +
+          (deltaX2 - min) +
+          " " +
+          0
+      );
     }
-    // draw tha pipe-like path
-    // 1. move a bit down, 2. arch,  3. move a bit to the right, 4.arch, 5. move down to the end 
-    path.attr("d",  "M"  + startX + " " + startY +
-                    " V" + (startY + delta) +
-                    " A" + delta + " " +  delta + " 0 0 " + arc1 + " " + (startX + delta*signum(deltaX)) + " " + (startY + 2*delta) +
-                    " H" + (endX - delta*signum(deltaX)) + 
-                    " A" + delta + " " +  delta + " 0 0 " + arc2 + " " + endX + " " + (startY + 3*delta) +
-                    " V" + endY );
+  } else if (startX > endX) {
+    if (startY < endY) {
+      //case 4: line bent down, then left, then down, then right again
+
+      path.attr(
+        "d",
+        "M" +
+          startX +
+          " " +
+          startY +
+          " l" +
+          offsetX +
+          " " +
+          0 +
+          " l" +
+          0 +
+          " " +
+          deltaY2 +
+          " l " +
+          -(deltaX + offsetX * 2) +
+          " " +
+          0 +
+          "  l" +
+          0 +
+          " " +
+          deltaY2 +
+          "  l" +
+          offsetX +
+          " " +
+          0
+      );
+    } else if (startY > endY) {
+      //case 5: line bent up, then left, then up, then right again
+      path.attr(
+        "d",
+        "M" +
+          startX +
+          " " +
+          startY +
+          " L" +
+          startX +
+          " " +
+          (startY + deltaY2) +
+          " L " +
+          endX +
+          " " +
+          (startY + deltaY2) +
+          " L " +
+          endX +
+          " " +
+          endY
+      );
+    } else if (startY == endY) {
+      //case 6: line bent up, then left, then down, then right again
+      path.attr(
+        "d",
+        "M" +
+          startX +
+          " " +
+          startY +
+          " l" +
+          offsetX +
+          " " +
+          0 +
+          " l" +
+          0 +
+          " " +
+          -offsetY +
+          " l " +
+          -(deltaX + 2 * offsetX) +
+          " " +
+          0 +
+          " l " +
+          0 +
+          " " +
+          offsetY
+      );
+    }
+  } else {
+    //case something went wrong: Draw a straight line ignoring all fancyness
+    console.log("ERROR: Unimplemented line path.");
+    path.attr("d", "M" + startX + " " + startY + " L" + endX + " " + endY);
+  }
 }
 
 /**
- * 
- * @param {*} svg 
- * @param {*} path 
- * @param {*} startElem 
- * @param {*} endElem 
- * @author alojzije https://gist.github.com/alojzije/11127839
+ *
+ * @param {*} svg
+ * @param {*} path
+ * @param {*} startElem
+ * @param {*} endElem
+
  */
 function connectElements(svg, path, startElem, endElem) {
-    var svgContainer= $("#svgContainer");
+  var svgContainer = $("#svgContainer");
 
-    // get (top, left) corner coordinates of the svg container   
-    var svgTop  = svgContainer.offset().top;
-    var svgLeft = svgContainer.offset().left;
+  // get (top, left) corner coordinates of the svg container
+  var svgTop = svgContainer.offset().top;
+  var svgLeft = svgContainer.offset().left;
 
-    // get (top, left) coordinates for the two elements
-    var startCoord = startElem.offset();
+  // get (top, left) coordinates for the two elements
+  var startCoord = startElem.offset();
 
-    var endCoord   = endElem.offset();
+  var endCoord = endElem.offset();
 
-    // calculate path's start (x,y)  coords
-    // we want the x coordinate to visually result in the element's mid point
-    var startX = startCoord.left + startElem.outerWidth() + 4;    // x = left offset + 0.5*width - svg's left offset
-    var startY = startCoord.top  + 0.5 * startElem.outerHeight() - svgTop;        // y = top offset + height - svg's top offset
+  // calculate path's start (x,y)  coords
+  // we want the x coordinate to visually result in the element's mid point
+  var startX = startCoord.left + startElem.outerWidth() + 4; // x = left offset + 0.5*width - svg's left offset
+  var startY = startCoord.top + 0.5 * startElem.outerHeight() - svgTop; // y = top offset + height - svg's top offset
 
-        // calculate path's end (x,y) coords
-    var endX = endCoord.left - svgLeft;
-    var endY = endCoord.top  + 0.5 * endElem.outerHeight() - svgTop;
+  // calculate path's end (x,y) coords
+  var endX = endCoord.left - svgLeft;
+  var endY = endCoord.top + 0.5 * endElem.outerHeight() - svgTop;
 
-    // call function for drawing the path
-    drawPath(svg, path, startX, startY, endX, endY);
-
+  // call function for drawing the path
+  drawPath(svg, path, startX, startY, endX, endY);
 }
 
-/** 
+/**
  * Creath a new SVG-Path with fixed style options and appends it to the given parent <svg>
  * @param {string} id The id of the new path. Should be unique. (This however is not enforced)
  * @param {string} parentId The parent of the new Node.
  */
 function createPath(id, parentId) {
-    const parentElement = document.getElementById(parentId);
-    const newPath = document.createElementNS(parentElement.namespaceURI,"path");
-    
-    newPath.setAttributeNS(null, "id", id);  
-    newPath.setAttributeNS(null, "d", "M0 0");  
-    newPath.setAttributeNS(null, "stroke", "black"); 
-    newPath.setAttributeNS(null, "stroke-width", "3px");  
-    newPath.setAttributeNS(null, "opacity", 1);  
-    newPath.setAttributeNS(null, "fill", "none");
-    newPath.setAttributeNS(null, "marker-end", "url(#pointer)");
-    newPath.setAttributeNS(null, "marker-start", "url(#start)");
+  const parentElement = document.getElementById(parentId);
+  const newPath = document.createElementNS(parentElement.namespaceURI, "path");
 
-    parentElement.appendChild(newPath);
+  newPath.setAttributeNS(null, "id", id);
+  newPath.setAttributeNS(null, "d", "M0 0");
+  newPath.setAttributeNS(null, "stroke", "black");
+  newPath.setAttributeNS(null, "stroke-width", "2px");
+  newPath.setAttributeNS(null, "opacity", 1);
+  newPath.setAttributeNS(null, "fill", "none");
+  newPath.setAttributeNS(null, "marker-end", "url(#pointer)");
+  newPath.setAttributeNS(null, "marker-start", "url(#start)");
 
+  parentElement.appendChild(newPath);
 }
 /**
  * Create a number of paths with {@link createPath()}
@@ -110,21 +286,21 @@ function createPath(id, parentId) {
  * @param {int} startingIndex Optional: The starting index.
  */
 function createPaths(n, parentId, startingIndex = 0) {
-    for (let index = startingIndex; index <= n + startingIndex; index++) {
-        createPath("path" + index, parentId);        
-    }
+  for (let index = startingIndex; index <= n + startingIndex; index++) {
+    createPath("path" + index, parentId);
+  }
 }
-
 
 /**
  * A list of connections to be drawn.
  */
 var connectionsList = [
-    {"from": "1", "to": "2"},
-    {"from": "2", "to": "3"},
-    {"from": "3", "to": "4"},
-    {"from": "4", "to": "5"},  
-    {"from": "5", "to": "6"},
+  { from: "1", to: "2" },
+  { from: "2", to: "3" },
+  { from: "3", to: "4" },
+  { from: "4", to: "5" },
+  { from: "5", to: "6" },
+  { from: "6", to: "6" },
 ];
 
 /**
@@ -133,48 +309,49 @@ var connectionsList = [
  * @param {*} to The End Point of the Connection.
  */
 function addConection(from, to) {
-    connectionsList.push({"from": from, "to": to});
-    console.log(connectionsList);
+  connectionsList.push({ from: from, to: to });
+  console.log(connectionsList);
 }
 
 //TO_DO: Refactor rename
 
 /**
  * Draws all the connections according to the {@link connectionsList}.
- * @param {*} parentId 
+ * @param {*} parentId
  */
 function connectAll(parentId) {
+  // reset svg each time
+  //   $("#" + parentId).attr("height", "0");
+  //   $("#" + parentId).attr("width", "0");
 
-    // reset svg each time 
-    $("#" + parentId).attr("height", "0");
-    $("#" + parentId).attr("width", "0");
+  //find out how many paths and connections we have
+  const pathCount = document.getElementById(parentId).childElementCount - 1; //TODO: take only <path> children into account
+  const connectionsCount = connectionsList.length;
 
-    //find out how many paths and connections we have
-    const pathCount = document.getElementById(parentId).childElementCount - 1;//TODO: take only <path> children into account 
-    const connectionsCount = connectionsList.length
+  if (pathCount < connectionsCount) {
+    //to few paths
+    createPaths(connectionsCount - pathCount, parentId, pathCount);
+  } else if (pathCount > connectionsCount) {
+    //too many paths
+    //TO_DO: Destroy unuesd paths
+  }
 
-    if (pathCount < connectionsCount) {
-        //to few paths
-        createPaths(connectionsCount - pathCount, parentId, pathCount)
-        
-    } else if (pathCount > connectionsCount) {
-        //too many paths
-        //TO_DO: Destroy unuesd paths
-    }
-
-    //draw each connection
-    for (let index = 0; index < connectionsList.length; index++) {
-        connectElements($("#" + parentId), $("#path" + index), $("#" + connectionsList[index].from), $("#" + connectionsList[index].to))
-        
-    }
-
+  //draw each connection
+  for (let index = 0; index < connectionsList.length; index++) {
+    connectElements(
+      $("#" + parentId),
+      $("#path" + index),
+      $("#" + connectionsList[index].from),
+      $("#" + connectionsList[index].to)
+    );
+  }
 }
 
-//add event handlers to the ready and resize events 
-$(document).ready(function() {
-    connectAll("svg1");
+//add event handlers to the ready and resize events
+$(document).ready(function () {
+  connectAll("svg1");
 });
 
 $(window).resize(function () {
-    connectAll("svg1");
+  connectAll("svg1");
 });
